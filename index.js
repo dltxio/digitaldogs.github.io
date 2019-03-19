dc4810b274d310143d19f38feed5a420c734487d
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2019-03-19 16:55:02 +1000

    Clean up

--- index.js
+++ index.js
@@ -106,23 +106,23 @@ app.post('/dog', (req, res) => {
         const name = req.body.name;
         const dob = req.baseUrl.dob;
         const microchip = req.body.microchip;
-        const dam = 0;
-        const sire = 0; 
-        const sex = 0;
-        const owner = '0x9e1525DA6AB3498dda99B97dc13E79f4c44b79d8';
+        const dam = req.body.dam;
+        const sire = req.body.sire; 
+        const sex = req.body.sex;
+        const owner = req.body.owner;
         const addPuppy = Abi.encodeMethod(add_abi, [name, dob, microchip, dam, sire, sex, owner]);
 
         const txParams = {
             nonce: 50,
             gasPrice: 2000000000, 
             gasLimit:  210000,
-            to: '0x3cfa8ea36fc9bef5c666af8a5fa2d27960cd030c', 
+            to: settings.Ethereum.ContractAddress, 
             value: '0x00', 
             data: addPuppy,
             chainId: 4
         }
         
-        var tx = Sign(txParams, '0x60f54928d665c30e3055863a7254d0eb9dc5d4aa14ef2b1af230085c690adada');
+        var tx = Sign(txParams, settings.Ethereum.PrivateKey);
         console.log(tx);
 
         eth.sendRawTransaction(tx).then((txHash) => {
@@ -168,7 +168,7 @@ app.get('/dogname', async (req, res) => {
 
 app.get('/dognames', async (req, res) => {
     const _count = await count();
-    console.log(_count);
+    console.log(Number(_count));
 
     const dogs = [];
     //var puppies = [];
