8c75d08822abed18c37327087dc34ac8d1ca3f9d
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2019-03-26 10:29:14 +1000

    Add breaders

---
+++ contracts/Breaders.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.0;
+
+import "./Ownable.sol";
+
+contract Breaders is Ownable {
+
+    mapping(bytes32 => address) _prefix;
+
+    function getOwner(string calldata prefix) external view returns (address) {
+        bytes32 _hash = keccak256(abi.encode(prefix));
+        return _prefix[_hash];
+    }
+
+    function addOwner(bytes32 prefixHash, address owner) external onlyOwner() {
+        _prefix[prefixHash] = owner;
+    }
+
+    function isOwned(bytes32 prefixHash) external view returns (bool) {
+        return false;
+    }
+}
