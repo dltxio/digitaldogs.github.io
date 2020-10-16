72eece9963321e86378e6a4a9be39b05083d5b46
Alex Xiang <xiangzhe0812@gmail.com>
2020-10-16 13:21:42 +1000

    Updates

---
+++ precache-manifest.ddc4ed21a58a0bab262e0adfc8618481.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3c62910590bd5e3fdde073c70d433c",
+    "url": "https://www.dltx.io/digitaldogs.github.io/index.html"
+  },
+  {
+    "revision": "700bf23c66f982e9378c",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "552087186124cd2a0378",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "700bf23c66f982e9378c",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/js/2.365473cd.chunk.js"
+  },
+  {
+    "revision": "e78e3739e4a45bfbf8fe24db19cad767",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/js/2.365473cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "552087186124cd2a0378",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/js/main.c7f28868.chunk.js"
+  },
+  {
+    "revision": "39f5d4c595f326282aa9",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/js/runtime-main.e740051d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://localhost:3000/precache-manifest.26ec21154fd2989d7c8b791ab100ebdb.js"
+  "https://www.dltx.io/digitaldogs.github.io/precache-manifest.ddc4ed21a58a0bab262e0adfc8618481.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://localhost:3000/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://www.dltx.io/digitaldogs.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
