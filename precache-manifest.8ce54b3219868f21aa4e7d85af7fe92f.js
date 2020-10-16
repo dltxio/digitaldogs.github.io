227301f7c1f484bfeb2929396efc9dc7b6b4b9f9
Alex Xiang <xiangzhe0812@gmail.com>
2020-10-16 13:41:22 +1000

    Updates

---
+++ precache-manifest.8ce54b3219868f21aa4e7d85af7fe92f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7a5571d46c782437be2e0655b3dcae",
+    "url": "https://www.dltx.io/digitaldogs.github.io/index.html"
+  },
+  {
+    "revision": "9ab1e65c929c10967b1f",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "38135b2905f607d27fc8",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "9ab1e65c929c10967b1f",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/js/2.5a357bb4.chunk.js"
+  },
+  {
+    "revision": "e78e3739e4a45bfbf8fe24db19cad767",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/js/2.5a357bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38135b2905f607d27fc8",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/js/main.0e3ec746.chunk.js"
+  },
+  {
+    "revision": "39f5d4c595f326282aa9",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/js/runtime-main.e740051d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://www.dltx.io/digitaldogs.github.io/precache-manifest.bc9ef67f38ca66facbf16a56312d27c0.js"
+  "https://www.dltx.io/digitaldogs.github.io/precache-manifest.8ce54b3219868f21aa4e7d85af7fe92f.js"
 );
 
 self.addEventListener('message', (event) => {
