e25204f44a05d4e1d8e391bb1f680badc083fc7a
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-11-01 08:10:46 +1000

    Update

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import "./css/stylish-portfolio.min.css"
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App" id="page-top">
       <header className="App-header">
         <h1>Digital Dogs</h1>
       </header>
       <div className="content">
+      <a className="menu-toggle rounded" href="#">
+        <i className="fas fa-bars"></i>
+      </a>
         <Register />
         <Dog />
       </div>
