12cdd33646973235e9cbf2f764e4ba3a084aee3c
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-11-08 20:42:14 +1000

    Update

--- src/components/TotalSupply.js
+++ src/components/TotalSupply.js
@@ -7,16 +7,18 @@ import setting from "../setting.json";
 const TotalSupply = async () => {
   const [supply, setSupply] = useState();
 
-  const web3 = new Web3(new Web3.providers.WebsocketProvider(setting.Ethereum.Node));
+  //https://rinkby.infura.io/v3/64710bd1f20c42519965cd9c1dab700b
+  //const web3 = new Web3(new Web3.providers.WebsocketProvider(setting.Ethereum.Node));
+  const web3 = new Web3(new Web3.providers.WebsocketProvider("https://rinkby.infura.io/v3/64710bd1f20c42519965cd9c1dab700b"));
 
   const contract = new web3.eth.Contract(
     dogsERC721.abi,
     setting.Ethereum.ContractAddress
   );
 
-  const result = await contract.methods.name();
-  console.log(result);
-  setSupply(result);
+  // const result = await contract.methods.name();
+  // console.log(result);
+  // setSupply(result);
   
   return (
     <h2>Total Supply {supply}</h2>
