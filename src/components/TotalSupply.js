f76be1b7b33768cc8d7fa5452c28fd4b5afc5b12
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-11-14 09:05:20 +1000

    Chore: Change to .env

--- src/components/TotalSupply.js
+++ src/components/TotalSupply.js
@@ -8,22 +8,12 @@ const TotalSupply = () => {
   const [supply, setSupply] = useState(0);
   const [breed, setBreed] = useState();
 
-  const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://rinkeby.infura.io/ws/v3/f22ec9acdf944e1eb2dc04ed2bea08e5"));
+  const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.REACT_APP_RINKEBY_URL));
 
   const contract = new web3.eth.Contract(
     dogsERC721.abi,
     setting.Ethereum.ContractAddress
   );
-
-  // contract.methods.totalSupply().call()
-  // .then(function(result){
-  //     setSupply(result)
-  // });
-
-  // contract.methods.name().call()
-  // .then(function(result){
-  //     setBreed(result)
-  // });
 
   const loadBreed = async () => {
     const result = await contract.methods.name().call();
