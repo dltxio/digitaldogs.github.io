1b10909675c0dda585cb8b0e266de4ed33c93e9c
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-11-14 09:02:14 +1000

    Chore:  Fix titles etc

--- src/components/TotalSupply.js
+++ src/components/TotalSupply.js
@@ -8,8 +8,6 @@ const TotalSupply = () => {
   const [supply, setSupply] = useState(0);
   const [breed, setBreed] = useState();
 
-  //https://rinkby.infura.io/v3/64710bd1f20c42519965cd9c1dab700b
-  //const web3 = new Web3(new Web3.providers.WebsocketProvider(setting.Ethereum.Node));
   const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://rinkeby.infura.io/ws/v3/f22ec9acdf944e1eb2dc04ed2bea08e5"));
 
   const contract = new web3.eth.Contract(
@@ -22,18 +20,25 @@ const TotalSupply = () => {
   //     setSupply(result)
   // });
 
-  contract.methods.name().call()
-  .then(function(result){
-      setBreed(result)
-  });
+  // contract.methods.name().call()
+  // .then(function(result){
+  //     setBreed(result)
+  // });
 
-  const loadCount = async () => {
+  const loadBreed = async () => {
     const result = await contract.methods.name().call();
+    console.log(result);
+    setBreed(result);
+  }
+
+  const loadSupply = async () => {
+    const result = await contract.methods.totalSupply().call();
     console.log(result);
     setSupply(result);
   }
 
-  loadCount();
+  loadSupply();
+  loadBreed();
 
   return (
     <h2>Total Supply {supply} of {breed}</h2>
