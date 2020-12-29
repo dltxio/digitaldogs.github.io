bc14ea5d39e977dec80c3fbd24fc9123d5867456
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-12-29 13:49:24 +1000

    Chore:  Add moment package

--- src/migrations/3_add_puppies.js
+++ src/migrations/3_add_puppies.js
@@ -1,8 +1,10 @@
 const DogERC721 = artifacts.require("DogERC721");
 const fs = require("fs");
 const neatCsv = require("neat-csv");
+const moment = require("moment");
 
-const filePath = "/Users/lucascullen/GitHub/dltxio/digitaldogs.github.io/src/migrations/beagles.csv";
+const filePath =
+  "/Users/lucascullen/GitHub/dltxio/digitaldogs.github.io/src/migrations/beagles.csv";
 let dogs = [];
 
 fs.readFile(filePath, async (error, data) => {
@@ -12,7 +14,7 @@ fs.readFile(filePath, async (error, data) => {
 
   const parsedData = await neatCsv(data);
 
-  parsedData.forEach(async (dog) => {
+  parsedData.forEach(async dog => {
     //console.log(dog);
     dogs.push(dog);
   });
@@ -22,12 +24,11 @@ module.exports = async (deployer, network, accounts) => {
   const contract = await DogERC721.deployed();
 
   if (dogs.length > 0) {
-    dogs.forEach(async dog => { 
+    dogs.forEach(async dog => {
       //console.log(dog);
       //console.log(dog.Name);
       const dob = moment(dog.DOB).format("YYYY-MM-DD");
       console.log(dob);
-
       //await contract.addPuppy("TEST", 0, "0x00", 0, 0, 0, accounts[0]);
       //await contract.addPuppy(dog.Name.toUpperCase(), 0, dog.Microchip, dog.Sex, dog.Dam, dog.Sire, dog.Owner);
     });
